fc6d73df411f57aa631d2b54e06fd5839b2ec654
Mercedes <m.er.chita@hotmail.com>
2021-03-26 23:41:08 -0300

    API skeleton

---
+++ API-Provinces/API-Provinces.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API_Provinces.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
