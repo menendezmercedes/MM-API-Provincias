5f232a5982946fd1c5f8368d7b1ba2a3884c17c2
Mercedes <m.er.chita@hotmail.com>
2021-03-31 22:24:16 -0300

    added provincescontroller unit test

---
+++ API-Provinces/API_Provinces.MTest/ProvincesControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API_Provinces.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Provinces.MTest
+{
+    [TestClass]
+    public class ProvincesControllerTest
+    {
+        [TestMethod]
+        public void InexistentProvince_GetProvince()
+        {
+             ProvincesController provincesGet = new ProvincesController();
+
+            var response = provincesGet.GetProvince("BadProvinceName");
+            var badResponse = response as BadRequestObjectResult;
+
+            // assert
+            Assert.IsNotNull(badResponse);
+            Assert.AreEqual(400, badResponse.StatusCode);
+        }
+        [TestMethod]
+        public void ExistentProvince_GetProvince()
+        {
+            ProvincesController provincesGet = new ProvincesController();
+
+            var response = provincesGet.GetProvince("Salta");
+            var okResponse = response as OkObjectResult;
+
+            // assert
+            Assert.IsNotNull(okResponse);
+            Assert.AreEqual(200, okResponse.StatusCode);
+        }
+    }
+}
